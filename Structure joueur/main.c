fe9e0514ed0cf08936b520049b9459b981c9cfa0
ValentinPasquini <96058776+ValentinPasquini@users.noreply.github.com>
2022-05-19 14:48:40 +0200

    Update main.c

--- Structure joueur/main.c
+++ Structure joueur/main.c
@@ -6,9 +6,9 @@ typedef struct joueur{
 int pv;             //nombre de PV du joueur
 int pa;             //nombre de PA du joueur
 int pm;             //nombre de PM du joueur
-int num_joueur;     //le numéro du joueur
+int num_joueur;     //le numÃ©ro du joueur
 int classe_joueur;  //(1: Mario, 2: Bowser, 3: Yoshi, 4: Toad)
-int pos_x;
-int pos_y;
+int pos_x;          //Position horizontale du joueur
+int pos_y;          //Position verticale du   joueur
 }t_joueur;
 ///-------------------Fin de la structure joueur-------------------///
