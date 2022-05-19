df17d8a65700adc79374e9c30cd00fa63ac69283
ValentinPasquini <96058776+ValentinPasquini@users.noreply.github.com>
2022-05-19 17:00:03 +0200

    Update initialisation_joueur.c

--- initialisation_joueur.c
+++ initialisation_joueur.c
@@ -1,1 +1,19 @@
-
+///------------------------Sous prog Initialisation des joueurs------------------------///
+                    //////////////////////////////////////////////////
+                    //      Le sous prog prend en paramètres:       //
+                    //          le nombre de joueurs (int)          //
+                    //      le tableau de structure tableau_joueur  //
+                    //////////////////////////////////////////////////
+int initialisation_joueur(int nbjoueurs, t_joueur* tableau_joueur)
+{
+    for(int i = 0; i<nbjoueurs; i++)
+    {
+        tableau_joueur[i].pv = 20;
+        tableau_joueur[i].pa = 3;
+        tableau_joueur[i].num_joueur = i;
+        tableau_joueur[i].classe_joueur = i+1;
+        tableau_joueur[i].pos_x = 0;
+        tableau_joueur[i].pos_y = 0;
+    }
+}
+///----------------------Fin sous prog Initialisation des joueurs----------------------///
