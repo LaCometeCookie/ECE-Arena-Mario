86d8f917a1ee80fbf0133a81238fea8dd236464b
ValentinPasquini <96058776+ValentinPasquini@users.noreply.github.com>
2022-05-19 16:43:49 +0200

    Create Structure_classe.c

---
+++ Structure_classe.c
@@ -0,0 +1,7 @@
+///---------------------------------Structure classe---------------------------------///
+typedef struct classe{
+char nom_de_classe[30];
+int tab_degats_attaques[5];     //tableau contenant les dégâts infligés par les attaques
+int tab_PA_attaques[5];         //tableau contenant les côuts des attaques
+}t_classe;
+///----------------------------Fin de la structure classe----------------------------///
