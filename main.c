4fb78cedfa2e352f54026c662931a6f6b0290d39
ValentinPasquini <96058776+ValentinPasquini@users.noreply.github.com>
2022-05-19 16:36:08 +0200

    Update and rename main to main.c

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <allegro.h>
+
+t_joueur *tableau_joueur = malloc(nbjoueurs * sizeof(t_joueur)); //Tableau joueurs, nécessite le nombre de joueurs: nbjoueurs
