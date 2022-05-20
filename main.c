20bb0b29f068ca8da0eed2b06a1914b4f4cc4ea9
ValentinPasquini <96058776+ValentinPasquini@users.noreply.github.com>
2022-05-20 11:17:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,13 @@
 ///---------------------------------------------Main---------------------------------------------///
 int main()
 {
-t_joueur *tableau_joueur = malloc(nbjoueurs * sizeof(t_joueur)); //Tableau joueurs, nécessite le nombre de joueurs: nbjoueurs
-t_classe *classe;
-initialisation_allegro();
+  initialisation_allegro();
+  t_joueur *tableau_joueur = malloc(nbjoueurs * sizeof(t_joueur)); //Tableau joueurs, nécessite le nombre de joueurs: "nbjoueurs"
+  t_classe *classe;
+  initialisation_allegro();
+  initialisation_joueur(nbjoueurs, t_joueur* tableau_joueur); //Initialise les joueurs, nécessite le nombre de joueurs: "nbjoueurs" et le tableau: "tableau_joueur"
+  ini_classe(t_classe *classe); //Initialise les classes, nécessite le tableau: "classe"
+
 return 0;
 }
 ///------------------------------------------Fin du Main------------------------------------------///
