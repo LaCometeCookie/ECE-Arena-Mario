23936058ec0817ec774dce7d446c587408e9ca3e
ValentinPasquini <96058776+ValentinPasquini@users.noreply.github.com>
2022-05-21 19:17:31 +0200

    Update initialisation_joueur.c

--- initialisation_joueur.c
+++ initialisation_joueur.c
@@ -1,3 +1,14 @@
+///----------------------------Structure joueur----------------------------///
+typedef struct joueur{
+int pv;             //nombre de PV du joueur
+int pa;             //nombre de PA du joueur
+int num_joueur;     //le numéro du joueur
+int classe_joueur;  //(1: Mario, 2: Bowser, 3: Yoshi, 4: Toad)
+int pos_x;          //Position horizontale du joueur
+int pos_y;          //Position verticale   du joueur
+}t_joueur;
+///-----------------------Fin de la structure joueur-----------------------///
+
 ///------------------------Sous prog Initialisation des joueurs------------------------///
                     //////////////////////////////////////////////////
                     //      Le sous prog prend en paramètres:       //
